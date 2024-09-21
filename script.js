a856061371304a2557ce6b3e70b692f82de44d6a
Raneem Rashid <raneemr05@hotmail.com>
2024-09-21 10:53:00 +0100

    Updated event handler code

--- script.js
+++ script.js
@@ -4,31 +4,31 @@ console.log("JS is loading successfully");
 document.title = "My Pizza Store";
 console.log(document.title);
 
-//Accessing a HTML element using its ID 
-let bannerText = document.getElementById("banner-text");
-console.log(bannerText);
+// //Accessing a HTML element using its ID 
+// let bannerText = document.getElementById("banner-text");
+// console.log(bannerText);
 
-// Changing the value of the element
-// innerText, innerHTML or textContent
-// bannerText.innerText = "<i> Serving Hot Pizzasss 🍕 </i>";
-// bannerText.innerHTML = "<i> Serving Hot Pizzasss 🍕 </i>"
-// bannerText.textContent = "Serving Hot Pizzasss 🍕";
+// // Changing the value of the element
+// // innerText, innerHTML or textContent
+// // bannerText.innerText = "<i> Serving Hot Pizzasss 🍕 </i>";
+// // bannerText.innerHTML = "<i> Serving Hot Pizzasss 🍕 </i>"
+// // bannerText.textContent = "Serving Hot Pizzasss 🍕";
 
-function changeText(){
-// Conditional text modification using DOM and JS based on the HTML element text
-if (bannerText.innerText === "Delicious Pizzas, Anytim") {
-    bannerText.innerText = "Serving Hot Pizzas";
-    // Manipulating the style using DOM style property
-    bannerText.style.color = "red";
-    bannerText.style.backgroundColor = "lightPink";
-} else {
-    bannerText.innerText = "No Pizza today";
-    bannerText.style.color = "blue";
-    bannerText.style.backgroundColor ="lightBlue";
-}
-}
+// function changeText(){
+// // Conditional text modification using DOM and JS based on the HTML element text
+// if (bannerText.innerText === "Delicious Pizzas, Anytim") {
+//     bannerText.innerText = "Serving Hot Pizzas";
+//     // Manipulating the style using DOM style property
+//     bannerText.style.color = "red";
+//     bannerText.style.backgroundColor = "lightPink";
+// } else {
+//     bannerText.innerText = "No Pizza today";
+//     bannerText.style.color = "blue";
+//     bannerText.style.backgroundColor ="lightBlue";
+// }
+// }
 
-changeText();
+// changeText();
 
 
 // ------ Changing the greeting message based on the username -----
@@ -75,37 +75,70 @@ let welcomeMessage = document.getElementById("welcome-message");
 // }
 
 
+
+
 // ------- DOM - DAY 2 ------
 // ---- getElementsByClassName ----
-let navLinks = document.getElementsByClassName('nav-link');
-console.log(navLinks);
-for (let index = 0; index < navLinks.length; index++) {
-   navLinks[index].style.color = "green";
-   navLinks[index].style.fontSize = "14px";
-}
+// let navLinks = document.getElementsByClassName('nav-link');
+// console.log(navLinks);
+// for (let index = 0; index < navLinks.length; index++) {
+//    navLinks[index].style.color = "green";
+//    navLinks[index].style.fontSize = "14px";
+// }
 
-// ---- getElementsByTagName and color all the h2 headings to Blue ----
+// // ---- getElementsByTagName and color all the h2 headings to Blue ----
 
 
-// ---- Attributes ----
-let offerImage = document.createElement("img");
-offerImage.setAttribute("src","https://plus.unsplash.com/premium_photo-1679924471066-dd984e92f395?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-console.log(offerImage.getAttribute("src"));
-// 1st Approach: Use inline styling 
-// offerImage.style.width = "300px";
-// offerImage.style.display = "block";
-// offerImage.style.margin = "0 auto";
+// // ---- Attributes ----
+// let offerImage = document.createElement("img");
+// offerImage.setAttribute("src","https://plus.unsplash.com/premium_photo-1679924471066-dd984e92f395?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+// console.log(offerImage.getAttribute("src"));
+// // 1st Approach: Use inline styling 
+// // offerImage.style.width = "300px";
+// // offerImage.style.display = "block";
+// // offerImage.style.margin = "0 auto";
 
-// 2nd Approach : Use CSS Class
-offerImage.classList.add("centered-image");
-console.log("Class name is: " + offerImage.className);
-// set its alternative text using alt
-offerImage.setAttribute("alt", "A delicious pizza for you!");
-console.log(offerImage.getAttribute('alt'));
-// Append it to the page
-document.body.appendChild(offerImage);
+// // 2nd Approach : Use CSS Class
+// offerImage.classList.add("centered-image");
+// console.log("Class name is: " + offerImage.className);
+// // set its alternative text using alt
+// offerImage.setAttribute("alt", "A delicious pizza for you!");
+// console.log(offerImage.getAttribute('alt'));
+// // Append it to the page
+// document.body.appendChild(offerImage);
 
 // ----- EVENTS -----
+// 1. Event Handling 
+// let viewButton = document.querySelectorAll('.cta-btn');
+// console.log(viewButton);
+// // let ctaBtn = document.getElementsByClassName('cta-btn');
+
+// for (let index = 0; index < viewButton.length; index++) {
+//     viewButton[index].onclick  = () => {
+//         console.log("Here you goo....");
+//     }
+// }
+
+
+// ---- EVENT HANDLER ----
+let viewButton = document.getElementById('view-btn');
+console.log(viewButton);
+
+viewButton.onclick = () =>{
+    console.log("I am clicked!");
+}
+
+viewButton.onclick = () =>{
+    console.log("I am clicked again!");
+}
+
+viewButton.onclick = () =>{
+    console.log("I am clicked again and again!");
+}
+
+
+
+
 
 
 
