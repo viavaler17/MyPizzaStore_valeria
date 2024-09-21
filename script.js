9c2f929490aff14869e868c5ee64384e69f0126c
Raneem Rashid <raneemr05@hotmail.com>
2024-09-21 10:00:18 +0100

    Updated code for day 2

--- script.js
+++ script.js
@@ -33,46 +33,79 @@ changeText();
 
 // ------ Changing the greeting message based on the username -----
 // Specify the username or leave it blank to simulate a guest
-let userName = "";
+let userName = "Valeria";
 let welcomeMessage = document.getElementById("welcome-message");
 
-// Conditional to check if userName is assigned or not 
-if(userName){
-    console.log(userName);
-    welcomeMessage.innerText = "Welcome, " + userName + "!";
-    //Creating an element using createElement method
-    let specialOffer = document.createElement('h1'); // <h1> </h1>
-    // Setting the content for the h2 element
-    specialOffer.innerText = "A Special Offer only for you!!!"; // <h1> A Special Offer only for you!!! </h1>
-    console.log(specialOffer); //testing if the element is created successfully
-    // Once the element is created we append it to the DOM / HTML Page
-    // Accessing the about element using the class property
-    // console.log(document.getElementsByClassName("about"));
-    let aboutSection = document.querySelector(".about");
-    // console.log(aboutSection);
-    aboutSection.insertAdjacentElement("afterend", specialOffer);
-    // Appending the newly created element to the body 
-    // document.body.appendChild(specialOffer);
+// // Conditional to check if userName is assigned or not 
+// if(userName){
+//     console.log(userName);
+//     welcomeMessage.innerText = "Welcome, " + userName + "!";
+//     //Creating an element using createElement method
+//     let specialOffer = document.createElement('h1'); // <h1> </h1>
+//     // Setting the content for the h2 element
+//     specialOffer.innerText = "A Special Offer only for you!!!"; // <h1> A Special Offer only for you!!! </h1>
+//     console.log(specialOffer); //testing if the element is created successfully
+//     // Once the element is created we append it to the DOM / HTML Page
+//     // Accessing the about element using the class property
+//     // console.log(document.getElementsByClassName("about"));
+//     let aboutSection = document.querySelector(".about");
+//     // console.log(aboutSection);
+//     aboutSection.insertAdjacentElement("afterend", specialOffer);
+//     // Appending the newly created element to the body 
+//     // document.body.appendChild(specialOffer);
 
-}
-else{
-    console.log(userName);
-    welcomeMessage.innerText = "Welcome Guest!";
-    // ------ Adding new element dynamically -----
-    // document.createElement('') then you add it to your DOM (appendChild)
-    let signUpButton = document.createElement('button');
-    signUpButton.innerText ="SIGN UP";
-    signUpButton.style.color = "red";
-    signUpButton.style.fontSize = "1.5em";
-    signUpButton.style.backgroundColor = "Orange";
-    signUpButton.style.padding = "5px";
-    let aboutSection = document.querySelector(".about");
-    aboutSection.insertAdjacentElement("afterend", signUpButton);
+// }
+// else{
+//     console.log(userName);
+//     welcomeMessage.innerText = "Welcome Guest!";
+//     // ------ Adding new element dynamically -----
+//     // document.createElement('') then you add it to your DOM (appendChild)
+//     let signUpButton = document.createElement('button');
+//     signUpButton.innerText ="SIGN UP";
+//     signUpButton.style.color = "red";
+//     signUpButton.style.fontSize = "1.5em";
+//     signUpButton.style.backgroundColor = "Orange";
+//     signUpButton.style.padding = "5px";
+//     let aboutSection = document.querySelector(".about");
+//     aboutSection.insertAdjacentElement("afterend", signUpButton);
 
-    // --- Remove an element ----
-    let specialitySection = document.querySelector('.speciality');
-    specialitySection.remove();
+//     // --- Remove an element ----
+//     let specialitySection = document.querySelector('.speciality');
+//     specialitySection.remove();
+// }
+
+
+// ------- DOM - DAY 2 ------
+// ---- getElementsByClassName ----
+let navLinks = document.getElementsByClassName('nav-link');
+console.log(navLinks);
+for (let index = 0; index < navLinks.length; index++) {
+   navLinks[index].style.color = "green";
+   navLinks[index].style.fontSize = "14px";
 }
+
+// ---- getElementsByTagName and color all the h2 headings to Blue ----
+
+
+// ---- Attributes ----
+let offerImage = document.createElement("img");
+offerImage.setAttribute("src","https://plus.unsplash.com/premium_photo-1679924471066-dd984e92f395?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+console.log(offerImage.getAttribute("src"));
+// 1st Approach: Use inline styling 
+// offerImage.style.width = "300px";
+// offerImage.style.display = "block";
+// offerImage.style.margin = "0 auto";
+
+// 2nd Approach : Use CSS Class
+offerImage.classList.add("centered-image");
+console.log("Class name is: " + offerImage.className);
+// set its alternative text using alt
+offerImage.setAttribute("alt", "A delicious pizza for you!");
+console.log(offerImage.getAttribute('alt'));
+// Append it to the page
+document.body.appendChild(offerImage);
+
+// ----- EVENTS -----
 
 
 
